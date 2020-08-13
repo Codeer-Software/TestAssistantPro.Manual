5144b57dd0d7cb8179fc22c8c88e6211c72216ab
HiroyukiKodama <kodama_soft@ybb.ne.jp>
2020-08-13 18:56:14 +0900

    ダイアログもMetroWindowで、SimpleDialogはバインディングを付ける

--- WPF/WpfDockApp/SimpleWindow.xaml.cs
+++ WPF/WpfDockApp/SimpleWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,11 +19,80 @@ namespace WpfDockApp
     /// <summary>
     /// SimpleWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class SimpleWindow : Window
+    public partial class SimpleWindow : MahApps.Metro.Controls.MetroWindow, INotifyPropertyChanged
     {
         public SimpleWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private string userName = string.Empty;
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                if (userName != value)
+                {
+                    userName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private DateTime? birthday ;
+
+        public DateTime? Birthday
+        {
+            get { return birthday; }
+            set
+            {
+                if (birthday != value)
+                {
+                    birthday = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string userLanguage;
+
+        public string UserLanguage
+        {
+            get { return userLanguage; }
+            set
+            {
+                if (userLanguage != value)
+                {
+                    userLanguage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string remarks;
+
+        public string Remarks
+        {
+            get { return remarks; }
+            set
+            {
+                if (remarks != value)
+                {
+                    remarks = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void OK_Click(object sender, RoutedEventArgs e)
